8c99878c5adc7a47caf1a8271b4a40bbfec25710
batrawi <batrawi2000forever@gmail.com>
2025-02-18 08:45:58 +0200

    create url pattern that treate with req params with get req

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def user(req, name, age):
+  content = f'<h1>Hello Boss</h1> <h2> The age of {name} is : {age}</h2>'
+  response = HttpResponse(content)
+  return response
+  
