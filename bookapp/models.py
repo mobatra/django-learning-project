e929f235beb263221998c10e8953f3308534fb99
batrawi <batrawi2000forever@gmail.com>
2025-02-20 05:59:15 +0200

    create and apply migration for book app

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  book_name = models.CharField(max_length=20)
+  release = models.IntegerField()
+  auth_name = models.CharField(max_length=20)
