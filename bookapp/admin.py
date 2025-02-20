863369c33ca55da6dcc71d19d590df4db497e8ea
batrawi <batrawi2000forever@gmail.com>
2025-02-20 11:38:11 +0200

    create CRUD with ORM for book and author models

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 
 admin.site.register(Book)
+admin.site.register(Author)
 
 # Register your models here.
--- bookapp/models.py
+++ bookapp/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+
+class Author(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+  def __str__(self):
+    return self.name
+  
 class Book(models.Model):
-  book_name = models.CharField(max_length=20)
-  release = models.IntegerField()
-  auth_name = models.CharField(max_length=20)
+  title = models.CharField(max_length=200, null=True, blank=True)
+  publication_date = models.DateField(null=True, blank=True)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name = "books",
+    null=True,
+    blank=True)
+  def __str__(self):
+    return self.title
